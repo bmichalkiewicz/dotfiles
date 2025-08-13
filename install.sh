bbf23bf3db3d12dc36ea334bafe514491a948311
Bartosz Michałkiewicz <bartosz.michalkiewicz@outlook.com>
2025-08-13 13:09:25 +0200

    chore: update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+sudo apt-get install -y build-essential procps curl file git unzip
+
 # https://github.com/ohmyzsh/ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
