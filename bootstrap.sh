b520c3782082b700a660691cc7b8e5591684e399
bmichalkiewicz <bartosz.michalkiewicz@outlook.com>
2025-08-13 14:34:16 +0200

    changes

--- bootstrap.sh
+++
@@ -1,9 +0,0 @@
-sudo apt-get install -y build-essential procps curl file git unzip zsh
-
-# https://brew.sh
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# https://github.com/ohmyzsh/ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-echo "Run commands from brew output"
--- install.sh
+++ install.sh
@@ -1,15 +1,24 @@
+sudo apt-get install -y build-essential procps curl file git unzip zsh
+
+# https://github.com/ohmyzsh/ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # https://github.com/zdharma-continuum/zinit?tab=readme-ov-file#install
 bash -c "$(curl --fail --show-error --silent \
     --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 
-# https://www.kcl-lang.io/docs/user_docs/getting-started/install#homebrew-macos-1
-brew install kcl-lang/tap/kcl-lsp
+# https://www.kcl-lang.io/docs/user_docs/getting-started/install#linux-1
+wget -q https://kcl-lang.io/script/install-kcl-lsp.sh -O - | /bin/bash
 
 # https://www.jetify.com/devbox/docs/installing_devbox/
 curl -fsSL https://get.jetify.com/devbox | bash
 
+# https://github.com/zyedidia/eget
+bash -c "$(curl --fail --show-error --silent \
+    --location https://zyedidia.github.io/eget.sh )"
+
 # https://github.com/hidetatz/kubecolor
-brew install kubecolor
+eget hidetatz/kubecolor
 
 # https://github.com/sharkdp/bat
-brew install bat
+eget sharkdp/bat
