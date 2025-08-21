955fef1330a36be4a5762ff71bfb13914a19f183
bmichalkiewicz <bartosz.michalkiewicz@outlook.com>
2025-08-22 00:10:30 +0200

    add docker and fix spf config

--- install.sh
+++ install.sh
@@ -26,12 +26,30 @@ TOOLS=(
   "mirceanton/kubectl-switch --asset amd64.tar.gz --to $LOCAL_BIN"
   "neovim/neovim --asset x86_64 --to $LOCAL_BIN/nvim"
   "jesseduffield/lazygit --to $LOCAL_BIN"
+  "jesseduffield/lazydocker --to $LOCAL_BIN"
 )
 
 for tool in "${TOOLS[@]}"; do
   "./eget" $tool
 done
 rm -rf eget
+
+# Docker
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+  sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 ### ZSH
 # Pure theme
