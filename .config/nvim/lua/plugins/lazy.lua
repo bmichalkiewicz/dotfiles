4d19504f9745d5d85d00b92d2d89fc56eb2306f0
bmichalkiewicz <bartosz.michalkiewicz@outlook.com>
2025-08-26 10:41:04 +0200

    chore: nvim update

--- .config/nvim/lua/plugins/lazy.lua
+++ .config/nvim/lua/plugins/lazy.lua
@@ -1,8 +1,20 @@
 return {
   {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    opts = {
+      trasparent_background = true,
+    },
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+      vim.cmd.colorscheme("catppuccin-macchiato")
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "catppuccin",
     },
   },
 }
